91e0bb0b95078baad0e6e5771c2bed6e298142b5
Aleksei Selutin <selutin.a@mail.ru>
2023-12-23 21:40:43 +0300

    добавил компонент 'часы'

--- src/components/Google/Google.jsx
+++ src/components/Google/Google.jsx
@@ -1,11 +1,14 @@
 import styles from "./Google.module.scss";
 
 import GoogleForm from "../GoogleForm/GoogleForm";
+import Time from "../Time/Time";
 
 function Google() {
   return (
     <section className={styles.main}>
-      <GoogleForm />
+      <div>
+        <GoogleForm />
+      </div>
     </section>
   );
 }
--- src/components/Time/Time.jsx
+++ src/components/Time/Time.jsx
@@ -1,7 +1,32 @@
 import styles from "./Time.module.scss";
+import React, { useState, useEffect } from "react";
 
 function Time() {
-  return;
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const hours = time.getHours().toString().padStart(2, "0");
+  const minutes = time.getMinutes().toString().padStart(2, "0");
+  const seconds = time.getSeconds().toString().padStart(2, "0");
+
+  return (
+    <>
+      <div className={styles.wrap}>
+        <span className={styles.Minutes}>{hours}</span>
+        <span className={styles.colonHours}>:</span>
+        <span className={styles.Minutes}>{minutes}</span>
+        <span className={styles.colonMinutes}>:</span>
+        <span className={styles.seconds}>{seconds}</span>
+      </div>
+    </>
+  );
 }
 
 export default Time;
