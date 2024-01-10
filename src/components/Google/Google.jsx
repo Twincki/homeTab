5527d9bb4f5380453e6155d09f4b091569dcc21e
Aleksei Selutin <selutin.a@mail.ru>
2024-01-10 18:03:17 +0300

    добавил некоторые базовые стили

--- src/components/Google/Google.jsx
+++ src/components/Google/Google.jsx
@@ -1,14 +1,20 @@
 import styles from "./Google.module.scss";
+import { VscAccount } from "react-icons/vsc";
 
 import GoogleForm from "../GoogleForm/GoogleForm";
 import Time from "../Time/Time";
 
 function Google() {
   return (
-    <section className={styles.main}>
-      <div>
+    <section className={styles.section}>
+      <header className={styles.header}>
+        <Time />
+        <h1 className={styles.title}>HomeTabe</h1>
+        <VscAccount className={styles.icon} />
+      </header>
+      <main className={styles.main}>
         <GoogleForm />
-      </div>
+      </main>
     </section>
   );
 }
--- src/components/GoogleForm/GoogleForm.jsx
+++ src/components/GoogleForm/GoogleForm.jsx
@@ -3,15 +3,13 @@ import styles from "./GoogleForm.module.scss";
 function GoogleForm() {
   return (
     <form className={styles.form} action="https://www.google.com/search">
-      <div>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Google"
-          id="query"
-          name="q"
-        />
-      </div>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Google"
+        id="query"
+        name="q"
+      />
     </form>
   );
 }
